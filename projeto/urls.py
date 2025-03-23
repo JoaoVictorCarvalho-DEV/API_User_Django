0183fa433207de5ff33f27555ba38ee3426d275e
João Víctor Miranda Carvalho <joao002miranda@gmail.com>
2025-03-23 18:00:25 -0300

    API configurada e templates atualizados

--- projeto/urls.py
+++ projeto/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 urlpatterns = [
-    path('admin', admin.site.urls),
-    path('sistema_auth', include('sistema_auth.urls')),
+    path('admin/', admin.site.urls),
+    path('site/', include('sistema_auth.urls')),#Essas são as URLs principais
 
-    path('auth_api', include('auth_manager_api.urls'))
+    path('auth_api/', include('auth_manager_api.urls'))#URLS da API
 ]
