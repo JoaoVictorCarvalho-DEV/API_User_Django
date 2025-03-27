16e2c9985c8428e99482062a00129faea0c20638
João Víctor Miranda Carvalho <joao002miranda@gmail.com>
2025-03-27 10:02:43 -0300

    Criação do modelo de ator

--- auth_manager_api/models.py
+++ auth_manager_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Atores(models.Model):
+    class Meta:
+        db_table = "atores"
+
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+    papel = models.CharField(max_length=50)
