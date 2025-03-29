d42993d57d7c17d0f80e769e1d8e584557d08c62
João Víctor Miranda Carvalho <joao002miranda@gmail.com>
2025-03-29 09:58:32 -0300

    Melhorias na api

--- auth_manager_api/models.py
+++ auth_manager_api/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+
+
+
 # Create your models here.
+class Atores(models.Model):
+    class Meta:
+        db_table = "atores"
+
+    papel = models.CharField(max_length=50)
+
+class Orgaos(models.Model):
+    class Meta:
+        db_table = "orgaos"
+    
+    nome = models.CharField(max_length=255, null=False)
+    sigla = models.CharField(max_length=10)
+
+
+class CustomUser(AbstractUser):
+    class Meta:
+        db_table ="users"
+
+    cpf = models.CharField(max_length=11, unique=True)
+    telefone = models.CharField(max_length=15)
+    atores_id = models.ForeignKey(Atores, on_delete=models.SET_NULL, null=True, blank=True)
+    orgao_id  = models.ForeignKey(Orgaos, on_delete=models.SET_NULL, null=True, blank=True)
+
+    REQUIRED_FIELDS = ['email', 'cpf']
+
+    def __str__(self):
+        return self.username
+    
+
+
+class Projetos(models.Model):
+    class Meta:
+        db_table = "projetos"
+    
+    nome = models.CharField(max_length=50, null=False)
+    descricao = models.CharField(max_length=255)
+    data_inicio = models.DateField()
+    data_final = models.DateField()
+    projeto_id = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
+    atores_id = models.ForeignKey(Atores, on_delete=models.SET_NULL, null=True, blank=True)
+
+class Tarefas(models.Model):
+    class Meta:
+        db_table = "tarefas"
+
+    nome = models.CharField(max_length=50, null=False)
+    descricao = models.CharField(max_length=255)
+    data_inicio = models.DateField()
+    data_final = models.DateField()
+    usuario_id = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
+    orgao_id = models.ForeignKey(Orgaos, on_delete=models.SET_NULL, null=True, blank=True)
+
--- projeto/settings.py
+++ projeto/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'auth_manager_api.CustomUser'
+
 
 # Application definition
 
