9650a6ecf88dc3ed7966f71834dcb32c98c5a88c
João Víctor Miranda Carvalho <joao002miranda@gmail.com>
2025-04-02 11:33:17 -0300

    Visualização e edição de Projetos feita

--- projeto_service/urls.py
+++ projeto_service/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import cadastrar_projeto, ver_projetos
+from .views import cadastrar_projeto, ver_projetos, editar_projeto
 
 urlpatterns = [
     path('cadastrar-projeto/', cadastrar_projeto, name="cadastrar_projeto"),
+
+    path('editar-projeto/<int:projeto_id>/', editar_projeto, name="editar_projeto"),
     path('ver-projetos/', ver_projetos, name="ver_projetos"),
 ]
