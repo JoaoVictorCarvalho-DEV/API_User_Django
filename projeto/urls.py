f6d60305f6280d1cee26a433ecdf330e84f3cb07
João Víctor Miranda Carvalho <joao002miranda@gmail.com>
2025-05-24 11:28:13 -0300

    feat: implementação da dashboard concluída.

--- projeto/urls.py
+++ projeto/urls.py
@@ -7,12 +7,12 @@ from auth_manager_api.urls import router
 urlpatterns = [
     path('admin/', admin.site.urls),
     #####SERVIÇOS#####
-    path('site/dashboard', include('dashboard.urls')),
+    path('', include('dashboard.urls')),# a primeira página a ser vista
     path('site/auth/', include('sistema_auth.urls')),#Essas são as URLs principais
     path('site/projetos/', include("projeto_service.urls")),
     path('site/tarefas/', include("tarefa_service.urls")),
     path('site/atores/', include("atores_service.urls")),
-    path('', include("atores_service.urls")), # a primeira página a ser vista
+
     path('site/orgaos/', include("orgao_service.urls")),
 
     #####API AUTENTICAÇÃO#####
